df042f1aba05cfdc1c233b620309e66fbca5b3fe
akien <akien@1c2a86c8-c92e-45aa-92f6-1ffee4d10753>
2008-03-07 20:11:40 +0000

    ajout de petites modifications.
    git-svn-id: svn+ssh://svn.tuxfamily.org/svnroot/informfr/jeux@18 1c2a86c8-c92e-45aa-92f6-1ffee4d10753

--- katabasis/trunk/katabasis.sh
+++ katabasis/trunk/katabasis.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-bin/inform-6.31-biplatform -v8 +Language_name=French +include_path=./,lib/ katabasis.inf katabasis.z8 | more
+../../../../outils/inform-6.31-biplatform -v8 +Language_name=French +include_path=./,../../../../lib/ katabasis.inf katabasis.z8 | more
 echo "Compilation terminee"
